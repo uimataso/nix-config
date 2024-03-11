f7996786542bc74a430f4213700bb2db5208be40
luck07051 <luck07051@gmail.com>
2024-03-11 22:08:21 +0800

    shell pkgs default?

--- shell.nix
+++ shell.nix
@@ -1,4 +1,14 @@
-{ pkgs }:
+{ pkgs ? # If pkgs is not defined, instanciate nixpkgs from locked commit
+  let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+  import nixpkgs { overlays = [ ]; }
+, ...
+}:
 
 {
   default = pkgs.mkShell {

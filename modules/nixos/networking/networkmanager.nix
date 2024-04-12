8a10919f8c4e7c20c0b1a3c9407bcc82f9f94e81
uima <luck07051@gmail.com>
2024-04-12 23:10:40 +0800

    networkmanager imper

--- modules/nixos/networking/networkmanager.nix
+++ modules/nixos/networking/networkmanager.nix
@@ -4,6 +4,7 @@ with lib;
 
 let
   cfg = config.myConfig.networking.networkmanager;
+  ifPersist = attrs: attrsets.optionalAttrs config.myConfig.system.impermanence.enable attrs;
 in
 {
   options.myConfig.networking.networkmanager = {
@@ -17,10 +18,10 @@ in
     systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
     systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 
-    # environment.persistence."/persist" = {
-    #   directories = [
-    #     "/etc/NetworkManager/system-connections"
-    #   ];
-    # };
+    environment.persistence.main = ifPersist {
+      directories = [
+        "/etc/NetworkManager/system-connections"
+      ];
+    };
   };
 }

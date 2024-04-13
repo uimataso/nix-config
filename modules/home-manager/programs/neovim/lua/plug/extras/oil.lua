450b5ff9cdb46ded6c43c6529478f906de830c26
uima <luck07051@gmail.com>
2024-04-13 20:29:06 +0800

    neovim not lazy on oil.nvim

--- modules/home-manager/programs/neovim/lua/plug/extras/oil.lua
+++ modules/home-manager/programs/neovim/lua/plug/extras/oil.lua
@@ -1,6 +1,6 @@
 return {
   'stevearc/oil.nvim',
-  event = 'Syntax',
+  lazy = false,
   keys = {
     { '<Leader>fi', function() require('oil').open() end,       desc = 'Open oil browser for a directory' },
     { '<Leader>fo', function() require('oil').open_float() end, desc = 'Open oil browser in a floating window' },

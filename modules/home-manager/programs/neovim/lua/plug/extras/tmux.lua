49a59a09a1cfbd226db5350fbcd0812f11a0a636
uima <luck07051@gmail.com>
2024-04-19 23:57:51 +0800

    tmux.nvim config

--- modules/home-manager/programs/neovim/lua/plug/extras/tmux.lua
+++ modules/home-manager/programs/neovim/lua/plug/extras/tmux.lua
@@ -1,4 +1,15 @@
 return {
   'aserowy/tmux.nvim',
   lazy = false,
+  opts = {
+    copy_sync = {
+      enable = true,
+    },
+    navigation = {
+      cycle_navigation = false,
+    },
+    resize = {
+      enable_default_keybindings = false,
+    }
+  }
 }

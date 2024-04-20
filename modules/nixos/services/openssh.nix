0c6a24f0d96187f25ebd0a6b1a5bbd2c0c5c52d4
uima <luck07051@gmail.com>
2024-04-20 22:14:11 +0800

    host ssh key persist, disable password auth

--- modules/nixos/services/openssh.nix
+++ modules/nixos/services/openssh.nix
@@ -16,11 +16,18 @@ in
     services.openssh = {
       enable = true;
       settings = {
+        PasswordAuthentication = false;
         PermitRootLogin = "no";
       };
     };
 
-    # TODO: how to manage key then?
-    environment.persistence.main = mkIf imper.enable { };
+    environment.persistence.main = mkIf imper.enable {
+      files = [
+        "/etc/ssh/ssh_host_rsa_key"
+        "/etc/ssh/ssh_host_rsa_key.pub"
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
+      ];
+    };
   };
 }

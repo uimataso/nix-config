9d89e0bb5e4c0063b7d7d72d4a2306094684166a
uima <luck07051@gmail.com>
2024-04-20 23:32:35 +0800

    disable ssh kbd auth

--- modules/nixos/services/openssh.nix
+++ modules/nixos/services/openssh.nix
@@ -17,6 +17,7 @@ in
       enable = true;
       settings = {
         PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
         PermitRootLogin = "no";
       };
     };

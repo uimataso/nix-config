1620d7a917d6201111494c0967266e388eb7de66
uima <luck07051@gmail.com>
2024-04-21 16:08:37 +0800

    sops age ssh gen key

--- modules/nixos/system/sops.nix
+++ modules/nixos/system/sops.nix
@@ -4,6 +4,10 @@ with lib;
 
 let
   cfg = config.uimaConfig.system.sops;
+
+  isEd25519 = k: k.type == "ed25519";
+  getKeyPath = k: k.path;
+  keys = builtins.filter isEd25519 config.services.openssh.hostKeys;
 in
 {
   options.uimaConfig.system.sops = {
@@ -16,6 +20,11 @@ in
 
   config = mkIf cfg.enable {
     sops = {
+      age = {
+        sshKeyPaths = map getKeyPath keys;
+        keyFile = "/var/lib/sops-nix/key.txt";
+        generateKey = true;
+      };
     };
   };
 }

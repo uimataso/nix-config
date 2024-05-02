9b039e7a0d946cb497cfdd4efd3e3a6c21839de4
uima <uimataso@proton.me>
2024-05-02 23:35:05 +0800

    add some aliases to nh, just for fun really

--- modules/home-manager/sh-util/nh.nix
+++ modules/home-manager/sh-util/nh.nix
@@ -18,9 +18,16 @@ in
     home.shellAliases = {
       no = "nh os switch ${flakeDir}";
       nt = "nh os test ${flakeDir}";
-      nr = "nix repl --expr \"builtins.getFlake \\\"${flakeDir}\\\"\"";
+      nr = ''nix repl --expr "builtins.getFlake \"${flakeDir}\""'';
+
+      nel = "sudo nix-env --list-generations -p /nix/var/nix/profiles/system | tail";
+
       nph = "nix profile history --profile /nix/var/nix/profiles/system";
       npd = "nix profile diff-closures --profile /nix/var/nix/profiles/system";
+      # remove no changes
+      nphs = ''nph | sed -z 's/Version [^ ]*[0-9]*[^ ]* ([0-9-]*) <- [0-9]*:\n\s*No changes.\n\n//g' '';
+      npds = ''npd | sed -z 's/Version [0-9]* -> [0-9]*:\n\n//g' '';
+
       it = "${pkgs.scripts.nix-template-tool}/bin/nix-template-tool";
     };
 

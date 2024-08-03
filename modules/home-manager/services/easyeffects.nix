f89cfc31bf912958d1018a5a438d2f15d2a18d7f
uima <uimataso@proton.me>
2024-08-03 16:52:50 +0800

    make easyeffects preset working

--- modules/home-manager/services/easyeffects.nix
+++ modules/home-manager/services/easyeffects.nix
@@ -4,6 +4,7 @@ with lib;
 
 let
   cfg = config.uimaConfig.services.easyeffects;
+  settings = config.services.easyeffects;
 
   imper = config.uimaConfig.system.impermanence;
 in
@@ -13,10 +14,26 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.easyeffects.enable = true;
+    services.easyeffects = {
+      enable = true;
+      preset = "masc_voice_noise_reduction";
+    };
 
-    home.persistence.main = mkIf imper.enable {
-      directories = [ ".config/easyeffects" ];
+    xdg.configFile = {
+      # Use [this](https://gist.github.com/jtrv/47542c8be6345951802eebcf9dc7da31) input preset. Change this values:
+      #   "rnnoise#0": {
+      #       "enable-vad": true,
+      #       "output-gain": 20.0,
+      #   },
+      "easyeffects/input/${settings.preset}.json".source = ./${settings.preset}.json;
+    };
+
+    # TODO: upstream this?
+    # Original command has --load-preset and --gapplication-service in one line,
+    # and that whill make --load-preset has not effect in my test.
+    systemd.user.services.easyeffects.Service = {
+      ExecStartPre = "${settings.package}/bin/easyeffects --load-preset ${settings.preset}";
+      ExecStart = mkForce "${settings.package}/bin/easyeffects --gapplication-service";
     };
   };
 }

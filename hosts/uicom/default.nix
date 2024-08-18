7b8e6bf212541ad8474156691bb569123bb6e2a7
uima <uimataso@proton.me>
2024-08-18 18:06:32 +0800

    inputClassSections is not good :(

--- hosts/uicom/default.nix
+++ hosts/uicom/default.nix
@@ -24,23 +24,23 @@
   #   };
   # };
 
-  services.xserver.inputClassSections = [
-    ''
-      Identifier      "ELECOM trackball catchall"
-      MatchProduct    "ELECOM TrackBall Mouse HUGE TrackBall"
-      MatchVendor     "ELECOM"
-      MatchIsPointer  "yes"
+  services.xserver.extraConfig = ''
+    Section "InputClass"
+      Identifier "ELECOM trackball catchall"
+      MatchProduct "ELECOM TrackBall Mouse HUGE TrackBall"
+      MatchVendor "ELECOM"
+      MatchIsPointer "yes"
       Driver "libinput"
-      Option "Buttons"              "12"
-      Option "ButtonMapping"        "1 2 2 4 5 6 7 8 12 10 11 3"
-      Option "EmulateWheel"         "true"
-      Option "EmulateWheelButton"   "9"
-      Option "EmulateWheelTimeout"  "0"
-      Option "ScrollMethod"         "button"
-      Option "ScrollButton"         "9"
-      Option "ScrollPixelDistance"  "32"
-    ''
-  ];
+      Option "Buttons" "12"
+      Option "ButtonMapping" "1 2 2 4 5 6 7 8 12 10 11 3"
+      Option "EmulateWheel" "true"
+      Option "EmulateWheelButton" "9"
+      Option "EmulateWheelTimeout" "0"
+      Option "ScrollMethod" "button"
+      Option "ScrollButton" "9"
+      Option "ScrollPixelDistance" "32"
+    EndSection
+  '';
 
   uimaConfig = {
     global.enable = true;

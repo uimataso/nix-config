8c9538b1635345e4c69729fec0d027718e463cfa
uima <uimataso@proton.me>
2024-08-25 18:18:38 +0800

    nvim: fix comment in openscad

--- modules/home-manager/programs/neovim/lua/plug/enhance/comment.lua
+++ modules/home-manager/programs/neovim/lua/plug/enhance/comment.lua
@@ -1,5 +1,7 @@
 return { -- I'm not really tested it, but I think this plugin has more function than buildin one (2024-08-24)
   'numToStr/Comment.nvim',
+  -- Sometime this won't load in `openscad`
+  event = 'VeryLazy',
   keys = {
     { 'gc', mode = { 'n', 'v' }, desc = 'Line comment' },
     { 'gb', mode = { 'n', 'v' }, desc = 'Block comment' },

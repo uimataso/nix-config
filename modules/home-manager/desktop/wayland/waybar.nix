cd0487601934dd7d5f2a80a57e55b379c52f1e11
uima <uimataso@proton.me>
2024-09-04 23:02:10 +0800

    disable stylix on waybar

--- modules/home-manager/desktop/wayland/waybar.nix
+++ modules/home-manager/desktop/wayland/waybar.nix
@@ -6,8 +6,6 @@
 with lib;
 let
   cfg = config.uimaConfig.desktop.wayland.waybar;
-
-  scheme = config.stylix.base16Scheme;
 in
 {
   options.uimaConfig.desktop.wayland.waybar = {
@@ -15,6 +13,8 @@ in
   };
 
   config = mkIf cfg.enable {
+    stylix.targets.waybar.enable = false;
+
     programs.waybar = {
       enable = true;
       systemd.enable = true;
@@ -207,6 +207,32 @@ in
           ];
         };
       };
+
+      style =
+        with config.lib.stylix.colors.withHashtag;
+        with config.stylix;
+        # css
+        ''
+        @define-color base00 ${base00}; @define-color base01 ${base01}; @define-color base02 ${base02}; @define-color base03 ${base03};
+        @define-color base04 ${base04}; @define-color base05 ${base05}; @define-color base06 ${base06}; @define-color base07 ${base07};
+
+        @define-color base08 ${base08}; @define-color base09 ${base09}; @define-color base0A ${base0A}; @define-color base0B ${base0B};
+        @define-color base0C ${base0C}; @define-color base0D ${base0D}; @define-color base0E ${base0E}; @define-color base0F ${base0F};
+
+        * {
+          font-family: "${fonts.sansSerif.name}";
+          font-size: ${builtins.toString fonts.sizes.desktop}pt;
+        }
+
+        window#waybar, tooltip {
+          background: alpha(@base00, ${builtins.toString opacity.desktop});
+          color: @base05;
+        }
+
+        tooltip {
+          border-color: @base0D;
+        }
+      '';
     };
   };
 }

54edc971924eb9e9c5d185ab85597aa66b24cb15
uima <uimataso@proton.me>
2024-09-05 23:43:24 +0800

    add colors list for nvim to display color

--- modules/home-manager/theme/default.nix
+++ modules/home-manager/theme/default.nix
@@ -93,3 +93,20 @@ in
     };
   };
 }
+
+# base00 = "#161616";
+# base01 = "#303030";
+# base02 = "#454545";
+# base03 = "#808080";
+# base04 = "#9b9b9b";
+# base05 = "#bcbcbc";
+# base06 = "#dddddd";
+# base07 = "#f5f5f5";
+# base08 = "#c68586";
+# base09 = "#edb96e";
+# base0A = "#d5be95";
+# base0B = "#86a586";
+# base0C = "#8caeaf";
+# base0D = "#83a0af";
+# base0E = "#d8afad";
+# base0F = "#b08b76";

3f23ba220db735222915292a1ff473431f1f1dff
uima <uimataso@proton.me>
2024-09-09 12:04:05 +0800

    update wsl

--- modules/nixos/system/wsl.nix
+++ modules/nixos/system/wsl.nix
@@ -13,10 +13,31 @@ in
   options.uimaConfig.system.wsl = {
     enable = mkEnableOption "WSL";
 
-    docker = mkOption {
+    docker.enable = mkOption {
       type = types.bool;
       default = true;
       description = "Enable Docker integration";
+    };
+
+    docker.enableOnBoot = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Started Docker on boot";
+    };
+
+    docker.package = mkOption {
+      type = types.package;
+      default = pkgs.docker;
+      defaultText = literalExpression "pkgs.docker";
+      description = ''
+        Docker package to use. Set to `null` to use the default package.
+      '';
+    };
+
+    defaultUser = mkOption {
+      type = types.str;
+      default = "uima";
+      description = "Default user";
     };
   };
 
@@ -25,16 +46,33 @@ in
   config = mkIf cfg.enable {
     wsl = {
       enable = true;
-      defaultUser = "uima";
-      docker-desktop.enable = cfg.docker;
+      defaultUser = cfg.defaultUser;
+      docker-desktop.enable = cfg.docker.enable;
     };
 
-    # Manual start dockerd
-    # TODO: Figure out a better other way to do this, but, this works :)
-    system.activationScripts = mkIf cfg.docker {
-      startDocker.text = ''
-        nohup ${pkgs.docker}/bin/dockerd > /tmp/docker.log 2> /tmp/docker.err.log &
-      '';
+    # Copy from [nixos modules](https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/virtualisation/docker.nix)
+    systemd.services.docker = mkIf cfg.docker.enable {
+      wantedBy = optional cfg.docker.enableOnBoot "multi-user.target";
+      after = [ "network.target" "docker.socket" ];
+      requires = [ "docker.socket" ];
+      environment = config.networking.proxy.envVars;
+      serviceConfig = {
+        Type = "notify";
+        ExecStart = "${cfg.docker.package}/bin/dockerd";
+        ExecReload = "${pkgs.procps}/bin/kill -s HUP $MAINPID";
+      };
     };
+
+    systemd.sockets.docker = mkIf cfg.docker.enable {
+      description = "Docker Socket for the API";
+      wantedBy = [ "sockets.target" ];
+      socketConfig = {
+        ListenStream = [ "/run/docker.sock" ];
+        SocketMode = "0660";
+        SocketUser = "root";
+        SocketGroup = "docker";
+      };
+    };
+
   };
 }

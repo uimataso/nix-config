a93709205a5571b3aae695c3c46c4471c89f7ae1
uima <uimataso@proton.me>
2024-09-09 23:44:23 +0800

    tofi style

--- modules/home-manager/programs/menu/tofi.nix
+++ modules/home-manager/programs/menu/tofi.nix
@@ -21,10 +21,23 @@ in
   };
 
   config = mkIf cfg.enable {
+    stylix.targets.tofi.enable = false;
+
     programs.tofi = {
       enable = true;
-      settings = {
-        background-color = mkForce "#000A";
+
+      # main tofi.5
+      settings = with config.lib.stylix.colors.withHashtag; {
+        font = config.stylix.fonts.monospace.name;
+        font-size = 20;
+
+        background-color = "#000A";
+        text-color = base05;
+        prompt-color = base0E;
+        selection-color = base0E;
+        selection-background = base01;
+        selection-background-padding = 4;
+        selection-background-corner-radius = 4;
 
         width = "100%";
         height = "100%";

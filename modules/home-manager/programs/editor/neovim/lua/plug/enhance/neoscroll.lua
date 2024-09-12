be873fc9dff32971f8738f30c2f656feb2d908c3
uima <uimataso@proton.me>
2024-09-13 01:06:46 +0800

    nvim: update nvimscroll

--- modules/home-manager/programs/editor/neovim/lua/plug/enhance/neoscroll.lua
+++ modules/home-manager/programs/editor/neovim/lua/plug/enhance/neoscroll.lua
@@ -1,31 +1,35 @@
 return {
   'karb94/neoscroll.nvim',
+
+  keys = {
+    { '<C-u>', function() require('neoscroll').ctrl_u({ duration = 150 }) end, desc = 'Neoscroll <C-u>' },
+    { '<C-d>', function() require('neoscroll').ctrl_d({ duration = 150 }) end, desc = 'Neoscroll <C-d>' },
+
+    { '<C-b>', function() require('neoscroll').ctrl_b({ duration = 250 }) end, desc = 'Neoscroll <C-b>' },
+    { '<C-f>', function() require('neoscroll').ctrl_f({ duration = 250 }) end, desc = 'Neoscroll <C-f>' },
+
+    {
+      '<C-y>',
+      function() require('neoscroll').scroll(-0.1, { move_cursor = false, duration = 100 }) end,
+      desc = 'Neoscroll <C-y>',
+    },
+    {
+      '<C-e>',
+      function() require('neoscroll').scroll(0.1, { move_cursor = false, duration = 100 }) end,
+      desc = 'Neoscroll <C-e>',
+    },
+
+    { 'zt', function() require('neoscroll').zt({ half_win_duration = 100 }) end, desc = 'Neoscroll zt' },
+    { 'zz', function() require('neoscroll').zz({ half_win_duration = 100 }) end, desc = 'Neoscroll zz' },
+    { 'zb', function() require('neoscroll').zb({ half_win_duration = 100 }) end, desc = 'Neoscroll zb' },
+
+    { '<PageUp>', '<C-u>', remap = true },
+    { '<PageDown>', '<C-d>', remap = true },
+  },
+
   opts = {
     mappings = {},
-  },
-  keys = {
-    { '<C-u>', function() require('neoscroll').scroll(-vim.wo.scroll, true, 150) end, desc = 'Neoscroll <C-u>' },
-    { '<C-d>', function() require('neoscroll').scroll(vim.wo.scroll, true, 150) end, desc = 'Neoscroll <C-d>' },
-
-    {
-      '<C-b>',
-      function() require('neoscroll').scroll(-vim.api.nvim_win_get_height(0), true, 250) end,
-      desc = 'Neoscroll <C-b>',
-    },
-    {
-      '<C-f>',
-      function() require('neoscroll').scroll(vim.api.nvim_win_get_height(0), true, 250) end,
-      desc = 'Neoscroll <C-f>',
-    },
-
-    { '<C-y>', function() require('neoscroll').scroll(-0.10, false, 50) end, desc = 'Neoscroll <C-y>' },
-    { '<C-e>', function() require('neoscroll').scroll(0.10, false, 50) end, desc = 'Neoscroll <C-e>' },
-
-    { 'zt', function() require('neoscroll').zt(100) end, desc = 'Neoscroll zt' },
-    { 'zz', function() require('neoscroll').zz(100) end, desc = 'Neoscroll zz' },
-    { 'zb', function() require('neoscroll').zb(100) end, desc = 'Neoscroll zb' },
-
-    { '<PageUp>', function() require('neoscroll').scroll(-vim.wo.scroll, true, 150) end, desc = 'Neoscroll <C-u>' },
-    { '<PageDown>', function() require('neoscroll').scroll(vim.wo.scroll, true, 150) end, desc = 'Neoscroll <C-d>' },
+    -- TODO: find the best one
+    -- easing = 'linear',
   },
 }

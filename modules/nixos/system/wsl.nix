6a4ffea85846ad438fa666c6bb72e7b0a135d40c
uima <uimataso@proton.me>
2024-09-18 11:22:46 +0800

    fix wsl docker integration

--- modules/nixos/system/wsl.nix
+++ modules/nixos/system/wsl.nix
@@ -19,21 +19,6 @@ in
       description = "Enable Docker integration";
     };
 
-    docker.enableOnBoot = mkOption {
-      type = types.bool;
-      default = true;
-      description = "Started Docker on boot";
-    };
-
-    docker.package = mkOption {
-      type = types.package;
-      default = pkgs.docker;
-      defaultText = literalExpression "pkgs.docker";
-      description = ''
-        Docker package to use. Set to `null` to use the default package.
-      '';
-    };
-
     defaultUser = mkOption {
       type = types.str;
       default = "uima";
@@ -43,40 +28,46 @@ in
 
   imports = [ inputs.nixos-wsl.nixosModules.default ];
 
-  config = mkIf cfg.enable {
-    wsl = {
-      enable = true;
-      defaultUser = cfg.defaultUser;
-      docker-desktop.enable = cfg.docker.enable;
-    };
-
-    # Copy from [nixos modules](https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/virtualisation/docker.nix)
-    systemd.services.docker = mkIf cfg.docker.enable {
-      description = "Docker Application Container Engine";
-      wantedBy = optional cfg.docker.enableOnBoot "multi-user.target";
-      after = [
-        "network.target"
-        "docker.socket"
-      ];
-      requires = [ "docker.socket" ];
-      environment = config.networking.proxy.envVars;
-      serviceConfig = {
-        Type = "notify";
-        ExecStart = "${cfg.docker.package}/bin/dockerd";
-        ExecReload = "${pkgs.procps}/bin/kill -s HUP $MAINPID";
+  config = mkIf cfg.enable (mkMerge [
+    {
+      wsl = {
+        enable = true;
+        defaultUser = cfg.defaultUser;
       };
-    };
+    }
 
-    systemd.sockets.docker = mkIf cfg.docker.enable {
-      description = "Docker Socket for the API";
-      wantedBy = [ "sockets.target" ];
-      socketConfig = {
-        ListenStream = [ "/run/docker.sock" ];
-        SocketMode = "0660";
-        SocketUser = "root";
-        SocketGroup = "docker";
+    # [See this](https://github.com/nix-community/NixOS-WSL/issues/235#issuecomment-1937424376)
+    (mkIf cfg.docker.enable {
+      wsl = {
+        # Enable integration with Docker Desktop (needs to be installed)
+        docker-desktop.enable = false;
+
+        # Binaries for Docker Desktop wsl-distro-proxy
+        extraBin = with pkgs; [
+          { src = "${coreutils}/bin/mkdir"; }
+          { src = "${coreutils}/bin/cat"; }
+          { src = "${coreutils}/bin/whoami"; }
+          { src = "${coreutils}/bin/ls"; }
+          { src = "${busybox}/bin/addgroup"; }
+          { src = "${su}/bin/groupadd"; }
+          { src = "${su}/bin/usermod"; }
+        ];
       };
-    };
 
-  };
+      virtualisation.docker = {
+        enable = true;
+        enableOnBoot = true;
+        autoPrune.enable = true;
+      };
+
+      ## patch the script
+      systemd.services.docker-desktop-proxy.script =
+        let
+          automountRoot = config.wsl.wslConf.automount.root;
+        in
+        lib.mkForce ''
+          ${automountRoot}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${automountRoot}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"
+        '';
+    })
+  ]);
 }

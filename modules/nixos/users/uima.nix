1dc13150beea17b2129965bd4eb4b90b8ab2b4f8
uima <git.m5svm@uimataso.com>
2024-12-11 23:14:52 +0800

    fix: remove initialPassword

--- modules/nixos/users/uima.nix
+++ modules/nixos/users/uima.nix
@@ -39,16 +39,7 @@ in
         ];
       }
 
-      # WARN:
-      # ┃ trace: evaluation warning: The user 'uima' has multiple of the options
-      # ┃ `hashedPassword`, `password`, `hashedPasswordFile`, `initialPassword`
-      # ┃ & `initialHashedPassword` set to a non-null value.
-      # ┃ The options silently discard others by the order of precedence
-      # ┃ given above which can lead to surprising results. To resolve this warning,
-      # ┃ set at most one of the options above to a non-`null` value.
-      # TODO: move this to impermanence module
       (mkIf imper.enable {
-        initialPassword = "password";
         hashedPasswordFile = "${imper.persist_dir}/passwords/${username}";
       })
     ];

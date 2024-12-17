504141343a4bed4dfd56458167b607090a80d5ca
uima <git.m5svm@uimataso.com>
2024-12-18 01:00:51 +0800

    nvim: try ts textobject, just copy the code from repo

--- modules/home-manager/programs/editor/neovim/lua/plug/treesitter.lua
+++ modules/home-manager/programs/editor/neovim/lua/plug/treesitter.lua
@@ -4,6 +4,7 @@ return {
   build = ':TSUpdate',
   dependencies = {
     'nvim-treesitter/playground',
+    'nvim-treesitter/nvim-treesitter-textobjects',
     -- parser for nu-lang
     'nushell/tree-sitter-nu',
   },
@@ -55,6 +56,36 @@ return {
       keymaps = {
         node_incremental = 'v',
         node_decremental = 'V',
+      },
+    },
+
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+          ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
+        },
+        selection_modes = {
+          ['@parameter.outer'] = 'v', -- charwise
+          ['@function.outer'] = 'V', -- linewise
+          ['@class.outer'] = '<c-v>', -- blockwise
+        },
+        include_surrounding_whitespace = true,
+      },
+
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>sa'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>sA'] = '@parameter.inner',
+        },
       },
     },
   },

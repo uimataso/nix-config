1645023a06fe73820818c2939a19828855211390
uima <git.m5svm@uimataso.com>
2024-12-18 00:04:33 +0800

    nvim: make lsp config cleaner

--- modules/home-manager/programs/editor/neovim/lua/plug/dev/lsp.lua
+++ modules/home-manager/programs/editor/neovim/lua/plug/dev/lsp.lua
@@ -1,3 +1,29 @@
+-- overwrite the diagnostic highgroup with custom one
+local overwrite_diagnostic_highlight = function(diagnostic_ns, highlight_ns, highlight_group)
+  vim.diagnostic.handlers[highlight_ns] = {
+    show = function(namespace, bufnr, diagnostics, _)
+      if namespace == diagnostic_ns then
+        for _, diagnostic in ipairs(diagnostics) do
+          vim.api.nvim_buf_add_highlight(
+            bufnr,
+            highlight_ns,
+            highlight_group,
+            diagnostic.lnum,
+            diagnostic.col,
+            diagnostic.end_col
+          )
+        end
+      end
+    end,
+
+    hide = function(namespace, bufnr)
+      if namespace == diagnostic_ns then
+        vim.api.nvim_buf_clear_namespace(bufnr, highlight_ns, 0, -1)
+      end
+    end,
+  }
+end
+
 return {
   {
     'neovim/nvim-lspconfig',
@@ -16,31 +42,7 @@ return {
           on_attach = function(client, _)
             local diagnostic_ns = vim.lsp.diagnostic.get_namespace(client.id)
             local typos_hl_ns = vim.api.nvim_create_namespace('typos_hl')
-
-            vim.diagnostic.handlers[typos_hl_ns] = {
-              show = function(namespace, bufnr, diagnostics, _)
-                if namespace ~= diagnostic_ns then
-                  return
-                end
-
-                for _, diagnostic in ipairs(diagnostics) do
-                  vim.api.nvim_buf_add_highlight(
-                    bufnr,
-                    typos_hl_ns,
-                    'SpellBad',
-                    diagnostic.lnum,
-                    diagnostic.col,
-                    diagnostic.end_col
-                  )
-                end
-              end,
-
-              hide = function(namespace, bufnr)
-                if namespace == diagnostic_ns then
-                  vim.api.nvim_buf_clear_namespace(bufnr, typos_hl_ns, 0, -1)
-                end
-              end,
-            }
+            overwrite_diagnostic_highlight(diagnostic_ns, typos_hl_ns, 'SpellBad')
           end,
         },
       },

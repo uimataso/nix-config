4a59afd1d03997b07c503508addfe248e477d335
uima <git.m5svm@uimataso.com>
2024-12-18 12:17:31 +0800

    nvim: todo keyword highlight

--- modules/home-manager/programs/editor/neovim/lua/plug/fuzzy/todo-comments.lua
+++ modules/home-manager/programs/editor/neovim/lua/plug/fuzzy/todo-comments.lua
@@ -9,5 +9,32 @@ return {
     { '<Leader>ft', '<cmd>TodoTelescope<cr>', desc = 'List all project todos in Tekescope' },
   },
 
-  opts = {},
+  opts = {
+    keywords = {
+      FIX = {
+        icon = ' ',
+        color = 'error',
+        alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' },
+      },
+      HACK = { icon = ' ', color = 'warning' },
+      WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+      TODO = { icon = ' ', color = 'todo' },
+      PERF = { icon = '󰔛 ', color = 'todo', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+      NOTE = { icon = '󰍨 ', color = 'note', alt = { 'INFO' } },
+      TEST = { icon = ' ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+    },
+    colors = {
+      error = { '@comment.error', 'DiagnosticError', 'ErrorMsg' },
+      warning = { '@comment.warning', 'DiagnosticWarn', 'WarningMsg' },
+      todo = { '@comment.todo', 'DiagnosticInfo' },
+      note = { '@comment.note', 'DiagnosticHint' },
+      test = { '@comment.warning', 'Identifier' },
+      default = { '@comment', 'Comment' },
+    },
+
+    -- match author also: KEYWORD(author):
+    -- https://github.com/folke/todo-comments.nvim/issues/10#issuecomment-2446101986
+    search = { pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]] },
+    highlight = { pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]] },
+  },
 }

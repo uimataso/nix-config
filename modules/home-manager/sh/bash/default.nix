50c44091a651bcdf4d6cff4dc249c55f860f6a08
uima <git.m5svm@uimataso.com>
2024-12-20 12:17:43 +0800

    bash: fix bashrc

--- modules/home-manager/sh/bash/default.nix
+++ modules/home-manager/sh/bash/default.nix
@@ -73,7 +73,7 @@ in
 
           # Readline
           bind 'set completion-ignore-case on'  # Completion case-insensitive
-          bind 'set completion-map-case on'     # Treat hypen and underscores as equivalent
+          bind 'set completion-map-case on'     # Treat hyphen and underscores as equivalent
 
           # Prompt
           prompt(){
@@ -108,11 +108,12 @@ in
             FZF_DEFAULT_OPTS="--height ''${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS" \
               $(__fzfcmd 2>/dev/null || echo fzf) -1 -0 --prompt "''${PS1@P}$line" --nth 2 -d "$_FZF_COMPLETION_SEP" --ansi \
               | tr -d "$_FZF_COMPLETION_SEP"
+          }
 
 
           # goto the nixstore of a executable
           ng() {
-            cd $(dirname $(readlink $(which $1)))
+            cd ''$(dirname ''$(readlink ''$(which ''$1)))
           }
         '';
     };

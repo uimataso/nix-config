9f18ac67e4bf56c1591af4c895c84bc22910f0ed
uima <git.m5svm@uimataso.com>
2025-01-09 19:02:19 +0800

    nvim: colorful menu update

--- modules/home-manager/programs/editor/neovim/lua/plug/completion.lua
+++ modules/home-manager/programs/editor/neovim/lua/plug/completion.lua
@@ -1,36 +1,3 @@
-local colorful_menu_draw = {
-  components = {
-    label = {
-      width = { fill = true, max = 60 },
-      text = function(ctx)
-        local highlights_info = require('colorful-menu').highlights(ctx.item, vim.bo.filetype)
-        if highlights_info ~= nil then
-          return highlights_info.text
-        else
-          return ctx.label
-        end
-      end,
-      highlight = function(ctx)
-        local highlights_info = require('colorful-menu').highlights(ctx.item, vim.bo.filetype)
-        local highlights = {}
-        if highlights_info ~= nil then
-          for _, info in ipairs(highlights_info.highlights) do
-            table.insert(highlights, {
-              info.range[1],
-              info.range[2],
-              group = ctx.deprecated and 'BlinkCmpLabelDeprecated' or info[1],
-            })
-          end
-        end
-        for _, idx in ipairs(ctx.label_matched_indices) do
-          table.insert(highlights, { idx, idx + 1, group = 'BlinkCmpLabelMatch' })
-        end
-        return highlights
-      end,
-    },
-  },
-}
-
 return {
   {
     'saghen/blink.cmp',
@@ -74,13 +41,28 @@ return {
 
       completion = {
         list = {
-          selection = 'auto_insert',
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
         },
         menu = {
           -- draw = {
           --   treesitter = { 'lsp' },
           -- },
-          draw = colorful_menu_draw,
+          draw = {
+            columns = { { 'kind_icon' }, { 'label', gap = 1 } },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require('colorful-menu').blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require('colorful-menu').blink_components_highlight(ctx)
+                end,
+              },
+            },
+          },
         },
         documentation = {
           auto_show = true,

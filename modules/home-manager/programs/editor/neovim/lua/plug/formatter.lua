2f2a63716b66b57e4bb3ed8291a01e969b7370b9
uima <git.m5svm@uimataso.com>
2025-02-11 13:39:18 +0800

    nvim: add `FormatDisable` and `FormatEnable` command

--- modules/home-manager/programs/editor/neovim/lua/plug/formatter.lua
+++ modules/home-manager/programs/editor/neovim/lua/plug/formatter.lua
@@ -1,3 +1,21 @@
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
 return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
@@ -24,10 +42,13 @@ return {
       nix = { 'nixfmt' }, -- nixfmt is official formatter but not yet stable, not sure which one to use
     },
 
-    format_on_save = {
-      lsp_format = 'fallback',
-      timeout_ms = 1000,
-    },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_format = 'fallback' }
+    end,
 
     formatters = {
       yamlfmt = {

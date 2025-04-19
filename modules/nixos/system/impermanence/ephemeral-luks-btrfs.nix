fde88d3c943cc2640adb1251f9d5b33df38129e9
uima <git.m5svm@uimataso.com>
2025-04-20 01:43:15 +0800

    try to fix ephemeral luks btrfs, now should works

--- modules/nixos/system/impermanence/ephemeral-luks-btrfs.nix
+++ modules/nixos/system/impermanence/ephemeral-luks-btrfs.nix
@@ -13,37 +13,47 @@ let
     ;
   cfg = config.uimaConfig.system.impermanence.luksBtrfs;
 
-  deviceName = "/dev/disk/by-partlabel/disk-main-luks";
-  systemdRootDevice = "dev-disk-by\\x2dpartlabel-disk\\x2dmain\\x2dluks.device";
+  luksName = "crypted";
 
   wipeScript =
     # sh
     ''
+      BTRFS_MNT='/btrfs_tmp'
+      BTRFS_SV='@'
+
+      error() {
+        echo "$@" >&2
+        exit 1
+      }
+
       delete_subvolume_recursively() {
         IFS=$'\n'
         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-          delete_subvolume_recursively "/btrfs_tmp/$i"
+          delete_subvolume_recursively "$BTRFS_MNT/$i"
         done
-        btrfs subvolume delete "$1"
+        btrfs subvolume delete "$1" || error "Failed to delete subvolume $1"
       }
 
-      mkdir /btrfs_tmp
-      mount ${deviceName} /btrfs_tmp
+      (
+        mount -m /dev/mapper/${luksName} "$BTRFS_MNT" || error 'Mount failed'
+        trap 'umount "$BTRFS_MNT"' EXIT
 
-      sv='@'
+        # Backup current subvolume
+        mkdir -p "$BTRFS_MNT/snapshots/$BTRFS_SV"
+        timestamp=$(date --date="@$(stat -c %Y "$BTRFS_MNT/$BTRFS_SV")" '+%Y-%m-%d_%H:%M:%S')
+        mv "$BTRFS_MNT/$BTRFS_SV" "$BTRFS_MNT/snapshots/$BTRFS_SV/$timestamp"
 
-      mkdir -p "/btrfs_tmp/snapshots/$sv"
-      timestamp=$(date --date="@$(stat -c %Y "/btrfs_tmp/$sv")" "+%Y-%m-%-d_%H:%M:%S")
-      mv "/btrfs_tmp/$sv" "/btrfs_tmp/snapshots/$sv/$timestamp"
+        # Remove the snapshots that is older than 7 day
+        find "$BTRFS_MNT/snapshots/$BTRFS_SV" -maxdepth 1 -mtime +7 | while IFS= read -r file; do
+          delete_subvolume_recursively "$file"
+        done
 
-      for i in $(find "/btrfs_tmp/snapshots/$sv" -maxdepth 1 -mtime +7); do
-        delete_subvolume_recursively "$i"
-      done
-
-      btrfs subvolume create "/btrfs_tmp/$sv"
-
-      umount /btrfs_tmp
+        # Create clean subvolume
+        btrfs subvolume create "$BTRFS_MNT/$BTRFS_SV" || error 'Failed to create subvolume'
+      )
     '';
+
+  devToSystemdDevice = dev: (lib.replaceStrings [ "-" "/" ] [ "\\x2d" "-" ] dev) + ".device";
 in
 {
   options.uimaConfig.system.impermanence.luksBtrfs = {
@@ -67,12 +77,14 @@ in
       systemd.services.restore-root = {
         description = "Rollback btrfs rootfs";
         wantedBy = [ "initrd.target" ];
-        requires = [ systemdRootDevice ];
-        after = [
-          systemdRootDevice
-          "systemd-cryptsetup@crypted.service"
-        ];
         before = [ "sysroot.mount" ];
+        requires = [
+          (devToSystemdDevice "dev/disk/by-partlabel/disk-main-luks")
+        ];
+        after = [
+          (devToSystemdDevice "dev/disk/by-partlabel/disk-main-luks")
+          "systemd-cryptsetup@${luksName}.service"
+        ];
         unitConfig.DefaultDependencies = "no";
         serviceConfig.Type = "oneshot";
         script = wipeScript;
@@ -106,7 +118,7 @@ in
             size = "100%";
             content = {
               type = "luks";
-              name = "crypted";
+              name = luksName;
 
               passwordFile = "/tmp/secret.key"; # Interactive
               # ref: https://mynixos.com/nixpkgs/options/boot.initrd.luks.devices.%3Cname%3E

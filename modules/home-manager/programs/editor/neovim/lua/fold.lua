5ffa31ddcbcabb13c8f1c76bf9bf68df51630f70
uima <git.m5svm@uimataso.com>
2025-04-29 18:49:38 +0800

    nvim: fold line

--- modules/home-manager/programs/editor/neovim/lua/fold.lua
+++ modules/home-manager/programs/editor/neovim/lua/fold.lua
@@ -6,8 +6,15 @@ vim.opt.foldtext = 'v:lua.Foldtext()'
 vim.opt.fillchars:append({ fold = ' ' })
 
 function Foldtext()
-  local line = require('utils').get_line_with_highlight(0, vim.v.foldstart - 1)
+  -- local line = require('utils').get_line_with_highlight(0, vim.v.foldstart - 1)
+  -- local lines_count = vim.v.foldend - vim.v.foldstart + 1
+  -- table.insert(line, { ('   󰁂 [ %d ] '):format(lines_count), 'MoreMsg' })
+  -- reutnr line
+
+  local line = vim.api.nvim_buf_get_lines(0, vim.v.foldstart - 1, vim.v.foldstart, false)[1]
   local lines_count = vim.v.foldend - vim.v.foldstart + 1
-  table.insert(line, { ('   󰁂 [ %d ] '):format(lines_count), 'MoreMsg' })
-  return line
+  return {
+    { line, 'Normal' },
+    { ('   󰁂 [ %d ] '):format(lines_count), 'MoreMsg' },
+  }
 end

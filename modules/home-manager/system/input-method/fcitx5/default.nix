a324e01011f8037a62cea4dbc07c45007909c604
uima <git.m5svm@uimataso.com>
2025-05-05 23:10:27 +0800

    cofig fcitx5

--- modules/home-manager/system/input-method/fcitx5/default.nix
+++ modules/home-manager/system/input-method/fcitx5/default.nix
@@ -15,36 +15,75 @@ in
 
   config = mkIf cfg.enable {
     i18n.inputMethod = {
-      enabled = "fcitx5";
+      enable = true;
+      type = "fcitx5";
+
       fcitx5.addons = with pkgs; [
         fcitx5-rime
+        fcitx5-mozc
       ];
     };
 
     home.sessionVariables = {
       "INPUT_METHOD" = "fcitx";
-      "GTK_IM_MODULE" = "fcitx";
+      # "GTK_IM_MODULE" = "fcitx";
       "QT_IM_MODULE" = "fcitx";
       "XMODIFIERS" = "@im=fcitx";
-      "GLFW_IM_MODULE" = "ibus";
-      "SDL_IM_MODULE" = "fcitx";
+      # "GLFW_IM_MODULE" = "ibus";
+      # "SDL_IM_MODULE" = "fcitx";
     };
 
-    # TODO: theme
+    # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland
+    gtk.gtk2.extraConfig = ''
+      gtk-im-module="fcitx"
+    '';
+    gtk.gtk3.extraConfig = {
+      gtk-im-module = "fcitx";
+    };
+    gtk.gtk4.extraConfig = {
+      gtk-im-module = "fcitx";
+    };
 
-    # ref:
-    # - https://github.com/search?q=nix+fcitx5&type=code
-    # - https://github.com/ryan4yin/nix-config/blob/82dccbdecaf73835153a6470c1792d397d2881fa/home/linux/gui/base/fcitx5/default.nix
-    # - https://github.com/ryan4yin/nix-config/blob/82dccbdecaf73835153a6470c1792d397d2881fa/overlays/fcitx5/README.md
-    # FIXME:
-    # xdg.configFile = {
-    #   "fcitx5/config".source = ./fcitx5-config;
-    #   "fcitx5/conf/classicui.conf".source = ./fcitx5-conf-classicui.conf;
-    #   "fcitx5/profile" = {
-    #     source = ./fcitx5-profile;
-    #     force = true;
-    #   };
-    # };
+    # FIXME: theme is different on firefox and other apps
+    i18n.inputMethod.fcitx5.settings = {
+      globalOptions = {
+        Behavior = {
+          ActiveByDefault = false;
+          resetStateWhenFocusIn = "No";
+          ShareInputState = "All";
+        };
+
+        Hotkey = {
+          EnumerateWithTriggerKeys = true;
+          EnumerateSkipFirst = false;
+          ModifierOnlyKeyTimeout = 250;
+        };
+
+        "Hotkey/TriggerKeys"."0" = "Control+Alt+Shift+I";
+        "Hotkey/TriggerKeys"."1" = "Super+I";
+        "Hotkey/PrevPage"."0" = "Up";
+        "Hotkey/NextPage"."0" = "Down";
+        "Hotkey/PrevCandidate"."0" = "Shift+Tab";
+        "Hotkey/NextCandidate"."0" = "Tab";
+        "Hotkey/TogglePreedit"."0" = "Control+Alt+P";
+      };
+
+      inputMethod = {
+        GroupOrder."0" = "Default";
+
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "rime";
+        };
+        "Groups/0/Items/0".Name = "keyboard-us";
+        "Groups/0/Items/1".Name = "rime";
+        "Groups/0/Items/2".Name = "mozc";
+      };
+
+      addons = {
+      };
+    };
 
     # Rime
     home.file.".local/share/fcitx5/rime" = {

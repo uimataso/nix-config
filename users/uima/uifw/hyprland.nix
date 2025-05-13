b35fdb031f0e1e6beaa62e3b1d4730fb95c6486e
uima <git.m5svm@uimataso.com>
2025-05-13 22:50:42 +0800

    uifw: double the idle time

--- users/uima/uifw/hyprland.nix
+++ users/uima/uifw/hyprland.nix
@@ -215,22 +215,22 @@ in
 
       listener = [
         {
-          timeout = 180; # 3min.
+          timeout = 480; # 8min.
           on-timeout = "brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
           on-resume = "brightnessctl -r"; # monitor backlight restore.
         }
         {
-          timeout = 300; # 5min
+          timeout = 600; # 10min
           on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
           on-resume = "loginctl unlock-session";
         }
         {
-          timeout = 330; # 5.5min
+          timeout = 660; # 11min
           on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
           on-resume = "hyprctl dispatch dpms on"; # screen on when activity is detected after timeout has fired.
         }
         {
-          timeout = 1800; # 30min
+          timeout = 3600; # 60min
           on-timeout = "systemctl suspend"; # suspend pc
         }
       ];

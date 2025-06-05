5977092834aa79b51e7cdbc9f9daa58b24409f08
uima <git.m5svm@uimataso.com>
2025-06-05 14:30:19 +0800

    fix foot that `tput: unknown terminal "foot"`

--- modules/home-manager/programs/terminal/foot.nix
+++ modules/home-manager/programs/terminal/foot.nix
@@ -33,6 +33,7 @@ in
       enable = true;
       settings = with config.lib.stylix.colors; {
         main = {
+          term = "xterm-256color";
           pad = "5x3";
         };
         cursor = {

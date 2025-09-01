d5f014e2f2de14e7643660f4b489aecfd5a3fae3
uima <git.m5svm@uimataso.com>
2025-09-01 12:37:11 +0800

    nvim: fix floating diagnostic didn't close when switch buffer

--- modules/home-manager/programs/editor/neovim/lua/lsp.lua
+++ modules/home-manager/programs/editor/neovim/lua/lsp.lua
@@ -27,6 +27,11 @@ for _, file in pairs(lsp_files) do
   vim.lsp.enable(lsp_name)
 end
 
+-- https://github.com/neovim/neovim/issues/35401
+vim.keymap.set('n', '<C-W>d', function()
+  vim.diagnostic.open_float(nil, { close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter' } })
+end, { desc = 'Show diagnostics under the cursor' })
+
 vim.diagnostic.config({
   virtual_text = false,
   update_in_insert = true,

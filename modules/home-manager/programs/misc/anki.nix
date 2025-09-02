742358301e58ed936cc9f0c9d12495be4723ae39
uima <git.m5svm@uimataso.com>
2025-09-02 12:04:56 +0800

    anki: try username/password and crowd-anki, no luck

--- modules/home-manager/programs/misc/anki.nix
+++ modules/home-manager/programs/misc/anki.nix
@@ -7,6 +7,8 @@
 let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.uimaConfig.programs.misc.anki;
+
+  dataDir = ".local/share/Anki2";
 in
 {
   options.uimaConfig.programs.misc.anki = {
@@ -16,9 +18,11 @@ in
   config = mkIf cfg.enable {
     uimaConfig.system.impermanence = {
       directories = [
-        ".local/share/Anki2"
+        dataDir
       ];
     };
+
+    stylix.targets.anki.enable = false;
 
     programs.anki = {
       enable = true;
@@ -26,16 +30,31 @@ in
 
       sync = {
         autoSync = true;
+        autoSyncMediaMinutes = 15;
+        usernameFile = "${config.home.homeDirectory}/${dataDir}/nix/username";
+        passwordFile = "${config.home.homeDirectory}/${dataDir}/nix/password";
         # TODO: sops secret?
         # secrets = config.sops.secrets;
         # usernameFile = mkIf (secrets ? "anki-username") secrets.anki-username.path;
-        # usernameFile = mkIf (secrets ? "anki-password") secrets.anki-password.path;
+        # passwordFile = mkIf (secrets ? "anki-password") secrets.anki-password.path;
       };
 
       theme = config.stylix.polarity;
 
       addons = [
         pkgs.ankiAddons.anki-connect
+        # (pkgs.anki-utils.buildAnkiAddon (finalAttrs: {
+        #   pname = "crowd-anki";
+        #   version = "20231030";
+        #   src = pkgs.fetchFromGitHub {
+        #     owner = "Stvad";
+        #     repo = "CrowdAnki";
+        #     rev = "907381102f22323e0a4f8eb49d536ad18de81ec5";
+        #     # sparseCheckout = [ "crowd_anki" ];
+        #     hash = "sha256-28DJq2l9DP8O6OsbNQCZ0pm4S6CQ3Yz0Vfvlj+iQw8Y=";
+        #   };
+        #   # sourceRoot = "${finalAttrs.src.name}/crowd_anki";
+        # }))
       ];
 
       answerKeys = [

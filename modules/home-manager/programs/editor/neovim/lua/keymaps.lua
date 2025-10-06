4ca943c9a6a4f1861a8316080597ad2ba97ffc8b
uima <git.m5svm@uimataso.com>
2025-10-06 21:12:12 +0800

    nvim: add keymap

--- modules/home-manager/programs/editor/neovim/lua/keymaps.lua
+++ modules/home-manager/programs/editor/neovim/lua/keymaps.lua
@@ -157,16 +157,24 @@ end, { silent = true, desc = 'Toggle conceal' })
 -- Treesitter inspect
 vim.keymap.set('n', '<Leader>hi', '<cmd>Inspect<cr>')
 
+-- Copy Full File-Path
+vim.keymap.set('n', '<leader>yf', function()
+  local path = vim.fn.expand('%:p')
+  vim.fn.setreg('+', path)
+  print('yank file path:', path)
+end)
+
 -- Copy text to clipboard with markdown codeblock format: ```{ft}{content}```
 vim.api.nvim_create_user_command('CopyCodeBlock', function(opts)
   local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, true)
   local content = table.concat(lines, '\n')
   local result = string.format('```%s\n%s\n```', vim.bo.filetype, content)
   vim.fn.setreg('+', result)
+  print('yank code block:', vim.bo.filetype)
 end, { range = true })
 vim.keymap.set(
   '',
-  '<Leader>cy',
+  '<Leader>yc',
   ':CopyCodeBlock<cr>',
   { desc = 'Copy text with markdown codeblock style' }
 )

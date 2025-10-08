ce89a0eb758db6d600847cb0ae20b6c385ba64ca
uima <git.m5svm@uimataso.com>
2025-10-08 11:30:03 +0800

    virt: remove qemu ovmf for migrate, see decs
    The 'virtualisation.libvirtd.qemu.ovmf' submodule has been removed. All
    OVMF images distributed with QEMU are now available by default.

--- modules/nixos/virt/vm.nix
+++ modules/nixos/virt/vm.nix
@@ -28,8 +28,6 @@ in
         enable = true;
         qemu = {
           swtpm.enable = true;
-          ovmf.enable = true;
-          ovmf.packages = [ pkgs.OVMFFull.fd ];
         };
       };
       spiceUSBRedirection.enable = true;

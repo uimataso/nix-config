6f22237b307fd284e39e8f4c6847abd423e201a8
uima <git.m5svm@uimataso.com>
2025-10-20 12:24:36 +0800

    uifw: enable avahi publish

--- hosts/uifw/default.nix
+++ hosts/uifw/default.nix
@@ -47,6 +47,15 @@
   services.avahi = {
     enable = true;
     nssmdns4 = true; # Allow .local hostnames to resolve
+
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
   };
 
   services.restic.backups = {

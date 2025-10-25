4ec7cce51d11a64f20453010db2af0b4929a426d
uima <git.m5svm@uimataso.com>
2025-10-25 18:50:24 +0800

    nvim: disable concel markdown code block

--- modules/home-manager/programs/editor/neovim/lua/treesitter.lua
+++ modules/home-manager/programs/editor/neovim/lua/treesitter.lua
@@ -13,3 +13,16 @@ ag('uima/Treesitter', function(g)
     end,
   })
 end)
+
+-- Disable default markdown conceal for code block
+-- from: https://github.com/nvim-treesitter/nvim-treesitter/issues/5751
+require('vim.treesitter.query').set(
+  'markdown',
+  'highlights',
+  [[
+;From MDeiml/tree-sitter-markdown
+[
+  (fenced_code_block_delimiter)
+] @punctuation.delimiter
+]]
+)

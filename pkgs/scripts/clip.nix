48c49f885a8ccd058cf5ecd339e45ac416dd986f
uima <git.m5svm@uimataso.com>
2025-11-08 20:01:06 +0800

    scripts: update clip

--- pkgs/scripts/clip.nix
+++ pkgs/scripts/clip.nix
@@ -23,6 +23,7 @@ writeShellApplication {
 
     Options:
       -h, --help                      Display this message.
+      -q, --quiet                     Don't print to stdout.
       -t, --type clipboard|primary    Clipboard type to use. Default: clipboard.
       -m, --mime mime/type            Set the MIME type.
     EOF
@@ -33,10 +34,11 @@ writeShellApplication {
       exit 1
     fi
 
+    quiet='''
     type='clipboard'
     mime='''
 
-    if TEMP=$(getopt -o 'ht:m:' -l 'help,type:,mime:' -n "$0" -- "$@"); then
+    if TEMP=$(getopt -o 'hqt:m:' -l 'help,quiet,type:,mime:' -n "$0" -- "$@"); then
       eval set -- "$TEMP"
       unset TEMP
     else
@@ -46,9 +48,10 @@ writeShellApplication {
 
     while true; do
         case "$1" in
-            '-h'|'--help')  help; exit ;;
-            '-t'|'--type')  type="$2"; shift 2 ;;
-            '-m'|'--mime')  mime="$2"; shift 2 ;;
+            '-h'|'--help')    help; exit ;;
+            '-q'|'--quiet')   quiet='true'; shift ;;
+            '-t'|'--type')    type="$2"; shift 2 ;;
+            '-m'|'--mime')    mime="$2"; shift 2 ;;
             '--') shift; break ;;
             *) debug "Internal error!" >&2; exit 1 ;;
         esac
@@ -57,51 +60,73 @@ writeShellApplication {
     session_type="''${XDG_SESSION_TYPE:-unknown}"
 
     case "$session_type" in
-      # NOTE: x11 is not tested
-      'x11')
-        opts="-selection $type"
-        [ -n "$mime" ] && opts="$opts -target $mime"
-
-        if [ ! -t 0 ]; then
-          # shellcheck disable=SC2086
-          cat | xclip -in $opts
-        fi
-
-        if [ -t 0 ] || [ ! -t 1 ]; then
-          # shellcheck disable=SC2086
-          xclip -out $opts
-        fi
-      ;;
-
-      'wayland')
-        opts='''
-
-        case "$type" in
-          'clipboard') ;;
-          'primary') opts="$opts --primary" ;;
-          *)
-            debug "Not supported clipboard type '$type'" >&2
-            exit 1
-          ;;
-        esac
-
-        [ -n "$mime" ] && opts="$opts --type $mime"
-
-        if [ ! -t 0 ]; then
-          # shellcheck disable=SC2086
-          cat | wl-copy $opts
-        fi
-
-        if [ -t 0 ] || [ ! -t 1 ]; then
-          # shellcheck disable=SC2086
-          wl-paste $opts
-        fi
-      ;;
-
+      'x11'|'wayland') ;;
       *)
         debug "Not supported session type '$XDG_SESSION_TYPE'" >&2
         exit 1
       ;;
     esac
+
+    xclip_opts() {
+      opts="-selection $type"
+      [ -n "$mime" ] && opts="$opts -target $mime"
+      printf '%s' "$opts"
+    }
+
+    wl_clipboard_opts() {
+      opts='''
+
+      case "$type" in
+        'clipboard') ;;
+        'primary') opts="$opts --primary" ;;
+        *)
+          debug "Not supported clipboard type '$type'" >&2
+          exit 1
+        ;;
+      esac
+
+      [ -n "$mime" ] && opts="$opts --type $mime"
+
+      printf '%s' "$opts"
+    }
+
+
+    copy() {
+      case "$session_type" in
+        'x11')
+          # shellcheck disable=SC2046
+          cat | xclip -in $(xclip_opts)
+          ;;
+        'wayland')
+          # shellcheck disable=SC2046
+          cat | wl-copy $(wl_clipboard_opts)
+          ;;
+      esac
+    }
+
+    paste() {
+      case "$session_type" in
+        'x11')
+          # shellcheck disable=SC2046
+          xclip -out $(xclip_opts)
+          ;;
+        'wayland')
+          # shellcheck disable=SC2046
+          wl-paste -n $(wl_clipboard_opts)
+          ;;
+      esac
+    }
+
+    if [ ! -t 0 ]; then
+      # trim newline at the end
+      cat | sed -z 's/\n$//' | copy
+    fi
+
+    if [ -z "$quiet" ]; then
+      paste
+
+      # print a newline if stdout is not pipe
+      [ -t 1 ] && printf '\n'
+    fi
   '';
 }

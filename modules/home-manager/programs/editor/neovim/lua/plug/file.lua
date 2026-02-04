7a6a559937e8d1c58dbf7ab03aba712436f2bb0f
uima <git@uimataso.com>
2026-02-04 11:21:23 +0800

    nvim: old.nvim

--- modules/home-manager/programs/editor/neovim/lua/plug/file.lua
+++ modules/home-manager/programs/editor/neovim/lua/plug/file.lua
@@ -1,10 +1,9 @@
 return {
   {
-    'A7Lavinraj/fyler.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    'stevearc/oil.nvim',
     lazy = false,
+    cmd = 'Oil',
 
-    cmd = 'Fyler',
     keys = {
       {
         '-',
@@ -13,34 +12,66 @@ return {
           if vim.v.count > 0 then
             vim.api.nvim_feedkeys(vim.v.count .. 'k', 'n', false)
           else
-            require('fyler').open({ kind = 'float' })
+            require('oil').open_float()
           end
         end,
-        desc = 'Open Fyler browser',
+        desc = 'Open Oil browser',
       },
     },
 
+    ---@type oil.SetupOpts
     opts = {
-      views = {
-        finder = {
-          default_explorer = true,
-          mappings = {
-            ['<Esc><Esc>'] = 'CloseView',
-            ['-'] = function(self)
-              vim.api.nvim_feedkeys(vim.v.count .. 'k', 'n', false)
-            end,
-          },
-          win = {
-            win_opts = {
-              number = true,
-              relativenumber = true,
-            },
-          },
+      win_options = {
+        signcolumn = vim.opt.signcolumn:get(),
+      },
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ['<Esc><Esc>'] = 'actions.close',
+        ['q'] = 'actions.close',
+        ['-'] = false,
+        ['_'] = false,
+      },
+      float = {
+        padding = 5,
+        max_width = 80,
+        max_height = 0,
+        win_options = {
+          winblend = 0,
         },
       },
-      integrations = {
-        icon = 'nvim_web_devicons',
-      },
     },
+
+    config = function(_, opts)
+      require('oil').setup(opts)
+
+      local au = require('utils').au
+      local ag = require('utils').ag
+
+      ag('uima/OilSettings', function(g)
+        au('FileType', {
+          group = g,
+          pattern = 'oil',
+          callback = function()
+            vim.keymap.set('n', '<BS>', '<Nop>', { buffer = 0 })
+            vim.keymap.set('n', '-', 'k', { buffer = 0 })
+          end,
+        })
+      end)
+
+      ag('uima/OilSnacksRename', function(g)
+        au('User', {
+          group = g,
+          pattern = 'OilActionsPost',
+          callback = function(event)
+            if event.data.actions.type == 'move' then
+              Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+            end
+          end,
+        })
+      end)
+    end,
   },
 }

3d41ffec56ff570d3fad10eaa6666ab225a5abb3
uima <git@uimataso.com>
2026-04-07 21:31:25 +0800

    nvim: only enable markdown for notes

--- modules/home-manager/programs/editor/neovim/lsp/marksman.lua
+++ modules/home-manager/programs/editor/neovim/lsp/marksman.lua
@@ -3,4 +3,16 @@ return {
   cmd = { 'marksman', 'server' },
   filetypes = { 'markdown', 'markdown.mdx' },
   root_markers = { '.marksman.toml', '.git' },
+
+  root_dir = function(bufnr, on_dir)
+    local notes_root = '/share/notes'
+    local buf_path = vim.api.nvim_buf_get_name(bufnr)
+    buf_path = vim.fn.fnamemodify(buf_path, ':p')
+
+    vim.print(buf_path)
+
+    if vim.startswith(buf_path, notes_root) then
+      on_dir(notes_root)
+    end
+  end,
 }

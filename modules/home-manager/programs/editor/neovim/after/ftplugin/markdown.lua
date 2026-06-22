c560ec1f15e7abc7f2a351527beaa885e9bc1a07
uima <git@uimataso.com>
2026-06-22 18:16:38 +0800

    nvim: format table

--- modules/home-manager/programs/editor/neovim/after/ftplugin/markdown.lua
+++ modules/home-manager/programs/editor/neovim/after/ftplugin/markdown.lua
@@ -1,1 +1,5 @@
 vim.o.conceallevel = 0
+
+-- format markdown table
+-- ref: https://heitorpb.github.io/bla/format-tables-in-vim/
+vim.keymap.set('x', '<leader>ft', ":! tr -s \" \" | column -t -s '|' -o '|'<cr>")

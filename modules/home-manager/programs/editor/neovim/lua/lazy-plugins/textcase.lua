b7e8373a0e2c33041a36a861d4b7d0eae297a7d4
uima <git@uimataso.com>
2026-08-20 14:47:50 +0800

    nvim: whichkey

--- modules/home-manager/programs/editor/neovim/lua/lazy-plugins/textcase.lua
+++ modules/home-manager/programs/editor/neovim/lua/lazy-plugins/textcase.lua
@@ -2,7 +2,35 @@ vim.pack.add({
   'https://github.com/johmsalas/text-case.nvim',
 })
 
--- TODO: use fuzzyfinder with preview instead
-require('textcase').setup({
-  prefix = '<leader>c',
+local textcase = require('textcase')
+local prefix = '<leader>c'
+
+textcase.setup({
+  prefix = prefix,
+  default_keymappings_enabled = false,
 })
+
+-- custom keymaps instead of textcase's defaults, for desc
+local cases = {
+  { method = 'to_constant_case', key = 'n', example = 'TO_CONSTANT_CASE' },
+  { method = 'to_camel_case', key = 'c', example = 'toCamelCase' },
+  { method = 'to_snake_case', key = 's', example = 'to_snake_case' },
+  { method = 'to_dash_case', key = 'd', example = 'to-dash-case' },
+  { method = 'to_pascal_case', key = 'p', example = 'ToPascalCase' },
+  { method = 'to_upper_case', key = 'u', example = 'TO UPPER CASE' },
+  { method = 'to_lower_case', key = 'l', example = 'to lower case' },
+}
+
+for _, c in ipairs(cases) do
+  vim.keymap.set({ 'n', 'x' }, prefix .. c.key, function()
+    textcase.quick_replace(c.method)
+  end, { desc = c.example })
+
+  vim.keymap.set('n', prefix .. c.key:upper(), function()
+    textcase.lsp_rename(c.method)
+  end, { desc = c.example .. ' (LSP)' })
+
+  vim.keymap.set('n', prefix .. 'o' .. c.key, function()
+    textcase.operator(c.method)
+  end, { desc = c.example .. ' (operator)' })
+end
---
+++ modules/home-manager/programs/editor/neovim/lua/plugins/which-key.lua
@@ -0,0 +1,32 @@
+vim.pack.add({
+  'https://github.com/folke/which-key.nvim',
+}, { load = true })
+
+require('which-key').setup({
+  preset = 'modern', -- "classic" | "modern" | "helix"
+  triggers = {
+    { '<leader>c', mode = { 'n', 'x' } },
+    { '<leader>a', mode = 'x' },
+  },
+  spec = {
+    { '<leader>a', mode = 'x', group = 'align' },
+    { '<leader>c', mode = { 'n', 'x' }, group = 'text-case' },
+    { '<leader>co', mode = 'n', group = 'operator' },
+    { '<leader>f', mode = 'n', group = 'find' }, -- fzf-lua + todo-comments
+    { '<leader>t', mode = 'n', group = 'toggle' }, -- gitsigns/spell/conceal toggles + run tests
+    { '<leader>/', mode = 'n', group = 'multicursor search' },
+    { '<leader>g', mode = { 'n', 'x' }, group = 'git' }, -- also has multicursor's ga/gv
+    { '<leader>d', mode = 'n', group = 'diagnostics/diff' },
+    { '<leader>D', mode = 'n', group = 'diagnostics (workspace)' },
+    { '<leader>y', mode = { 'n', 'x' }, group = 'yank' },
+    { '<leader>r', mode = 'n', group = 'run' }, -- RunCmd + rust-analyzer actions
+    { '<leader>h', mode = 'n', group = 'help' },
+    { '<leader>s', mode = { 'n', 'x' }, group = 'swap/substitute' }, -- also multicursor skip-cursor leaf
+    { '<leader>w', mode = 'n', group = 'wayfinder' },
+    { '<leader><leader>', mode = 'n', group = 'buffer-wide' }, -- whole-buffer yank/run
+  },
+})
+
+vim.keymap.set('n', '<leader>?', function()
+  require('which-key').show({ global = false })
+end, { desc = 'which key' })
